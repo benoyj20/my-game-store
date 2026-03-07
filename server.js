b5c7dc743c8ff1c7c0cb7938d01868542d39498f
Benoy Joseph <benoyj13@gmail.com>
2026-03-07 15:31:36 -0500

    Secure database connection using environment variables

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "tiger",
-  database: "game_application",
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 28698,
+  ssl: { rejectUnauthorized: true }
 });
 
 db.connect(err => {
